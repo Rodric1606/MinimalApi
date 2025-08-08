248f3e382aa31b00eac9e3961a33ad52d07bab71
Rodrigo S. Cardozo <rodric1606@hotmail.com>
2025-08-08 17:39:41 -0300

    Configuração admin e EndPoit login

--- obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e348f339e133c67a313a722db544817d94cdab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f95d1d720ec2c408ea830337aabe9d7ede8cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
