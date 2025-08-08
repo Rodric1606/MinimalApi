5c0e35a59484f0a0ea420ec0a92a55d30a18b57d
Rodrigo S. Cardozo <rodric1606@hotmail.com>
2025-08-08 15:09:24 -0300

    Atualização

--- obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a934b6483e4752e6ddd3afa2a27e075ae6ee81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8223b572e42bf781c37509fc7aa65e4f76d15182")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
